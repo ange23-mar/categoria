7de146cae8a1cf74c0adfa5747e334853c972fb1
Angelica Araya <ange.arayav@duocuc.cl>
2026-06-29 00:22:02 -0400

    Correcion yaml

--- src/main/java/com/example/ms_categorias/MsCategoriasApplication.java
+++ src/main/java/com/example/ms_categorias/MsCategoriasApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsCategoriasApplication {
+public static void main(String[] args) {
+      System.setProperty("TNS_ADMIN_PATH", "C:/Users/Usuario/Desktop/prueba3FullStrack/ms-proveedores/src/main/resources/Wallet");
+      SpringApplication.run(MsCategoriasApplication.class, args);
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsCategoriasApplication.class, args);
-	}
-
+}
 }
